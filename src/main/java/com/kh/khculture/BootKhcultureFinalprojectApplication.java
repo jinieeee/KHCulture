f79a7e7bae7571042d0d4bf962da08e234d6e7ec
jammmmin2 <yjhada9@naver.com>
2021-11-09 15:47:58 +0900

    11/9 진짜

---
+++ src/main/java/com/kh/khculture/BootKhcultureFinalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.kh.khculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootKhcultureFinalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootKhcultureFinalprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/khculture/BootKhcultureFinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.khculture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootKhcultureFinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
