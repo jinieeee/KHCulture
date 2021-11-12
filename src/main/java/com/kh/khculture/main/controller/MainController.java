1f124df41572d2f09c52146b7c51e9e3cd763564
jinieeee <lje911@gmail.com>
2021-11-12 23:50:14 +0900

    member html commit and push

--- src/main/java/com/kh/khculture/main/controller/MainController.java
+++ src/main/java/com/kh/khculture/main/controller/MainController.java
@@ -2,12 +2,17 @@ package com.kh.khculture.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 	
 	@GetMapping(value= {"/", "/main"})
 	public String main() {
-		return "main/main";
+		return "main";
+	}
+	@PostMapping(value="/")
+	public String redirectMain() {
+		return "redirect:/";
 	}
 }
