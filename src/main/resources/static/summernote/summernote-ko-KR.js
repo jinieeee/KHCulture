451ad872ae2bf868c3a8348053d22efd4ac7ca7d
gitYJ0815 <jdudwo@gmail.com>
2021-11-18 09:08:21 +0900

    page add...

--- src/main/resources/static/summernote/summernote-ko-KR.js
+++ src/main/resources/static/summernote/summernote-ko-KR.js
@@ -2,7 +2,7 @@
  * 
  * Super simple wysiwyg editor v0.8.18
  * https://summernote.org
- * 
+ *  
  * 
  * Copyright 2013- Alan Hong. and other contributors
  * summernote may be freely distributed under the MIT license.
