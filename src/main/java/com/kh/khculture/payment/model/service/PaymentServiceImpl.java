dfec0d7e299e94992d3a5337f6332ae63660f1ac
gitYJ0815 <jdudwo@gmail.com>
2021-11-23 14:16:42 +0900

    lectureDetail add jyj

--- src/main/java/com/kh/khculture/payment/model/service/PaymentServiceImpl.java
+++ src/main/java/com/kh/khculture/payment/model/service/PaymentServiceImpl.java
@@ -1,13 +1,31 @@
 package com.kh.khculture.payment.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.kh.khculture.lecture.model.service.LectureServiceImpl;
+import com.kh.khculture.payment.model.dao.PaymentMapper;
+import com.kh.khculture.payment.model.vo.LectureDetail;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 public class PaymentServiceImpl implements PaymentService{
+	
+	private PaymentMapper paymentMapper;
+	
+	@Autowired
+	public PaymentServiceImpl(PaymentMapper paymentMapper) {
+		
+		this.paymentMapper = paymentMapper;
+	}
+	
+	@Override
+	public LectureDetail selectOneLectureOpen(int lrNo) {
+		
+		return paymentMapper.selectOneLectureOpen(lrNo);
+	}
+	
+	
 
 }
